cf7d2c8aeccbb53726c691f6250ab39c4aed53ce
yowon <yowon0425@naver.com>
2025-02-15 14:40:28 +0900

    blog page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import PostList from './PostList';
+import PostDetail from './PostDetail';
+import AddPost from './AddPost';
 
 function App() {
+  const [selectedPost, setSelectedPost] = useState(null);
+  const [posts, setPosts] = useState([
+    { id: 1, title: "첫 번째 게시물", imageUrl: "https://image.dongascience.com/Photo/2020/03/5bddba7b6574b95d37b6079c199d7101.jpg", content: "화난 강아디", timestamp: new Date().toISOString() },
+    { id: 2, title: "두 번째 게시물", imageUrl: "https://i.namu.wiki/i/d1A_wD4kuLHmOOFqJdVlOXVt1TWA9NfNt_HA0CS0Y_N0zayUAX8olMuv7odG2FiDLDQZIRBqbPQwBSArXfEJlQ.webp", content: "고양이 조아", timestamp: new Date().toISOString() },
+  ]);
+  const [isAddingPost, setIsAddingPost] = useState(false);
+
+  const handleAddPost = (newPost) => {
+    setPosts([newPost, ...posts]);
+    setIsAddingPost(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Blog</h1>
+      {selectedPost ? (
+        <PostDetail post={selectedPost} onClose={() => setSelectedPost(null)} />
+      ) : (
+        <PostList posts={posts} onSelectPost={setSelectedPost} />
+      )}
+      {isAddingPost && <AddPost onAddPost={handleAddPost} onClose={() => setIsAddingPost(false)} />}
+      <button className="add-button" onClick={() => setIsAddingPost(true)}>추가</button>
     </div>
   );
 }
